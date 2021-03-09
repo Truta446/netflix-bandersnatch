8371043fe94d0dc7918aa616ad2d66878927bafb
Juan Versolato Lopes <juan.versolato@outlook.com>
2021-03-09 15:22:15 -0400

    Task: Show video on stream and compute time.

--- script.sh
+++ script.sh
@@ -2,7 +2,7 @@ ASSETSFOLDER=assets/timeline
 
 for mediaFile in `ls $ASSETSFOLDER | grep .mp4`; do
     # Corta a extensão e a resolução do arquivo
-    FILENAME=$(echo $mediaFile | sed -n 's/.mp4//p' || sed -n 's/-1920x1080//p')
+    FILENAME=$(echo $mediaFile | sed -n 's/.mp4//p' | sed -n 's/-1920x1080//p')
     INPUT=$ASSETSFOLDER/$mediaFile
     FOLDER_TARGET=$ASSETSFOLDER/$FILENAME
     mkdir -p $FOLDER_TARGET
